608041251d3d85475f0c70a5e3238b2ca39dc7d8
mhyche <mikaenkh@gmail.com>
2025-02-07 10:30:06 +0800

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+package checkstyledemo;
+
+/**
+ * Даалгавар: "Hello, World!" гэж хэвлэх энгийн Java програм бичсэн.
+ */
+
+public class Main {
+  public static void main(String []args) {
+    System.out.println("Hello, World!");
+  }
+}
